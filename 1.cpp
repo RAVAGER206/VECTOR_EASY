7e89f46f5d2f18c98e5e1c458acedcce340d80cf
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2023-02-06 22:17:21 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -16,7 +16,7 @@ mass2.push_back(mass[i]);
 
 void itc_even_parts_list(const vector <int>& mass, vector <int>& mass2) // 2
 {
-for (int i = 0; i < mass.size(); i ++)
+for (int i = 0; i < mass.size(); i++)
 if (mass[i] % 2 == 0)
 mass2.push_back(mass[i]);
 } // end
