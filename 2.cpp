156bc1705a714492caeb6ce6f823f309c631772f
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2023-02-12 19:34:38 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -6,52 +6,87 @@
 using namespace std;
 
 
-void itc_rev_list(vector <int>& mass) // 6
+void itc_rev_list(vector <int> &mass) // 6
 {
-if (mass.size() == 0) 
+if(mass.size()==0)
 {
-}
-else 
+}else
 {
-for (int i = 0; i < mass.size() / 2; i++) 
+for(int i = 0; i < mass.size()/2; i++)
 {
-mass[i] = mass[i] + mass[mass.size() - i - 1];
-mass[mass.size() - i - 1] = mass[i] - mass[mass.size() - i - 1];
-mass[i] = mass[i] - mass[mass.size() - i - 1];
+mass[i] = mass[i] + mass[mass.size()-i-1];
+mass[mass.size()-i-1] = mass[i] - mass[mass.size()-i-1];
+mass[i] = mass[i] - mass[mass.size()-i-1];
 }
 }
 } // end
 
 
-void itc_rev_par_list(vector <int>& mass) // 7
+void itc_rev_par_list(vector <int> &mass) // 7
 {
-if (mass.size() == 0) 
+if(mass.size()==0)
 {
 }
-else 
+else
 {
-for (int i = 0; i < mass.size() - mass.size() % 2; i += 2) 
+for(int i = 0; i < mass.size() - mass.size() % 2; i+=2)
 {
 mass[i] = mass[i] + mass[i + 1];
-mass[i++] = mass[i] - mass[i++];
+mass[i + 1] = mass[i] - mass[i + 1];
 mass[i] = mass[i] - mass[i + 1];
 }
 }
 } // end
 
 
-void itc_rshift_list(vector <int>& mass) // 8
+void itc_rshift_list(vector <int> &mass) // 8
 {
-if (mass.size() == 0) 
+if(mass.size()==0)
 {
 }
-else 
+else
 {
-int a = mass[mass.size() - 1];
-for (int i = mass.size() - 1; i > 0; i--) 
+int a = mass[mass.size()-1];
+for(int i=mass.size()-1; i>0;i--)
+{
+mass[i] = mass[i-1];
+}
+mass[0] = a;
+}
+} // end
+
+
+void itc_super_shift_list(vector <int> &mass, int n) // 9
+{
+int a;
+if(mass.size()==0)
+{
+}
+else
+{
+if (n > 0){
+for(int i = 0; i < n; i++)
+{
+a = mass[ mass.size() - 1];
+for(int i =  mass.size() - 1; i > 0; i--)
 {
 mass[i] = mass[i - 1];
 }
 mass[0] = a;
+}
+}
+if (n < 0)
+{
+n = n*-1;
+for( int i = 0; i < n; i++)
+{
+a = mass[0];
+for(int i = 0; i <  mass.size() - 1; i++)
+{
+mass[i] = mass[i + 1];
+}
+mass[ mass.size() - 1] = a;
+}
+}
 }
 } // end
