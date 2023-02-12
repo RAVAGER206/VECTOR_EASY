d3a5c556dfaa6218f675ef6f7e2d53fcf4124ecb
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2023-02-12 19:29:12 +0300

    Update easy_list.h

--- easy_list.h
+++ easy_list.h
@@ -1,6 +1,7 @@
 #ifndef EASY_LIST_H_INCLUDED
 #define EASY_LIST_H_INCLUDED
 
+
 #include <iostream>
 #include <vector>
 #include "easy_list.h"
@@ -9,21 +10,22 @@
 using namespace std;
 
 
-void itc_even_index_list (const vector <int>& mass, vector <int>& mass2); // 1
+void itc_even_index_list (const vector <int> &mass, vector <int> &mass2);
 
-void itc_even_parts_list (const vector <int>& mass, vector <int>& mass2); // 2
+void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2);
 
-int itc_positive_list (const vector <int>& mass); // 3
+int itc_positive_list(const vector <int> &mass);
 
-int itc_sl_list (const vector <int>& mass); // 4
+int itc_sl_list(const vector <int> &mass);
 
-bool itc_same_parts_list (const vector <int>& mass); // 5
+bool itc_same_parts_list(const vector <int> &mass);
 
-void itc_rev_list (vector <int>& mass); // 6
+void itc_rev_list(vector <int> &mass);
 
-void itc_rev_par_list (vector <int>& mass); // 7
+void itc_rev_par_list(vector <int> &mass);
 
-void itc_rshift_list (vector <int>& mass); // 8
+void itc_rshift_list(vector <int> &mass);
 
+void itc_super_shift_list(vector <int> &mass, int n);
 
-#endif // end
+#endif // EASY_LIST_H_INCLUDED
